8d412bbafd95b0eca9c83672b6a8859055573202
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-07-23 10:10:23 -0600

    done ralf

--- service.js
+++ service.js
@@ -1,12 +1,19 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+  	var deferred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response){
+    	var parsedResponse = response.data.data;
+    	for(var i = 0; i < parsedResponse.length; i++){
+    		parsedResponse[i].first_name = 'Ralf';
+    	}
+    	deferred.resolve(parsedResponse);
     })
+    return deferred.promise;
   }
-
 });
